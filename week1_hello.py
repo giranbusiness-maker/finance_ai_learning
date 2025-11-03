06c7982aff9d32dbbfbad91fc92779b111a219d7
giranbusiness-maker <giranbusiness@gmail.com>
2025-11-03 20:24:34 +0100

    first commit

--- week1_hello.py
+++ week1_hello.py
@@ -1,7 +1,1 @@
-print("Hello, future AI-Finance builder!")
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/felhasznaloneved/finance-ai-learning.git
-git push -u origin main
+
